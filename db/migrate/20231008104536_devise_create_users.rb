df335d4d41031bc1f70502b0e1f9685080841f55
sakuuuuuuuu <themesong99@gmail.com>
2024-01-05 22:11:44 +0900

    マイグレーションファイル調整

--- db/migrate/20231008104536_devise_create_users.rb
+++ db/migrate/20231008104536_devise_create_users.rb
@@ -4,14 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,               null: false
-      t.string :email,              null: false, default: ""
+      t.string :nickname          , null: false
+      t.string :email             , null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name_last         , null: false 
-      t.string :name_first        ,null: false 
+      t.string :name_first        , null: false 
       t.string :name_last_kana    , null: false 
       t.string :name_first_kana   , null: false 
-      t.date :birth_day         , null: false 
+      t.date :birth_day           , null: false 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -45,7 +45,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps null: false
     end
 
-    
+
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
