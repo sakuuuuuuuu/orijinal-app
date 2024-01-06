6455376e87248a9cf8e3ef2c4d9c1ac2749f6945
sakuuuuuuuu <themesong99@gmail.com>
2024-01-06 15:20:58 +0900

    マイグレーションファイルを美しく

--- db/migrate/20231018083243_create_items.rb
+++ db/migrate/20231018083243_create_items.rb
@@ -1,16 +1,16 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string  :name, null: false
-      t.text  :explaination, null: false
-      t.integer  :category_id, null: false   
-      t.integer  :condition_id, null: false
-      t.integer  :del_fee_id, null: false
+      t.string     :name,                  null: false
+      t.text       :explaination,          null: false
+      t.integer    :category_id,           null: false   
+      t.integer    :condition_id,          null: false
+      t.integer    :del_fee_id,            null: false
       
-      t.integer :prefecture_id, null: false
-      t.integer  :days_until_shipping_id, null: false
-      t.integer  :price, null: false
-      t.references  :user, null: false, foreign_key: true
+      t.integer    :prefecture_id,         null: false
+      t.integer    :days_until_shipping_id,null: false
+      t.integer    :price,                 null: false
+      t.references :user, null: false,     foreign_key: true
     
 
       t.timestamps
