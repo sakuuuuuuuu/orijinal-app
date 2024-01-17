6d8c3e9ee1dbbd3999ed34cd5450db406a70d2f5
sakuuuuuuuu <themesong99@gmail.com>
2024-01-17 21:12:23 +0900

    アクティブストレージ

--- db/migrate/20231018092424_create_active_storage_tables.active_storage.rb
+++ db/migrate/20231018092424_create_active_storage_tables.active_storage.rb
@@ -40,7 +40,6 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
     create_table :active_storage_variant_records, id: primary_key_type do |t|
       t.belongs_to :blob, null: false, index: false, type: foreign_key_type
       t.string :variation_digest, null: false
-
       t.index [ :blob_id, :variation_digest ], name: :index_active_storage_variant_records_uniqueness, unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
