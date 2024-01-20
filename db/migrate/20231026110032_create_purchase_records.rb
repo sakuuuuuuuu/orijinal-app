0df23f41167a92f04b87eb1579d4960b0bb02a27
sakuuuuuuuu <themesong99@gmail.com>
2024-01-20 23:31:19 +0900

    マイグレーションファイル

--- db/migrate/20231026110032_create_purchase_records.rb
+++ db/migrate/20231026110032_create_purchase_records.rb
@@ -3,20 +3,14 @@ class CreatePurchaseRecords < ActiveRecord::Migration[7.0]
     # remove_column :purchase_records, :price,:string
 
     create_table :purchase_records do |t|
-    
-
-
-
+  
 
       t.references :user  ,null: false, foreign_key: true
-      t.references :item  ,null: false, foreign_key: true
-
-      
-    
-      
-    
+      t.references :item  ,null: false, foreign_key: true    
 
       t.timestamps
     end
   end
 end
+
+
