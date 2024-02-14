f08d4792ab5ebb4c45d979a663cbbba7d2cf8948
sakuuuuuuuu <themesong99@gmail.com>
2024-02-14 23:17:51 +0900

    調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,12 @@ class ApplicationController < ActionController::Base
 
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
-     
-
   end
 
-    def configure_permitted_parameters
+  def configure_permitted_parameters
       # devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation, :name_last, :name_first, :name_last_kana, :name_first_kana, :birth_day])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_last, :name_first, :name_last_kana,:name_first_kana,:birth_day])
-    end
+  end
 end
 
 
