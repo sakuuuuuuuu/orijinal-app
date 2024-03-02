80718f50a0484036e27368bb790f76444c6daf65
sakuuuuuuuu <themesong99@gmail.com>
2024-03-02 22:37:20 +0900

    ルーター

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # get 'posts', to: 'posts#index'
+
   root to: 'items#index'
   # resources :users, only: [:edit, :update]
   resources :users
   resources :items do
   # root to: 'purchase_records#index'
+  
   resources :purchase_records, only:[:create,:index]
   end
 end
