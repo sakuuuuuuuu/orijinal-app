08c62c3415879c3285a091d26cde2bf1a07e71ad
sakuuuuuuuu <themesong99@gmail.com>
2024-03-09 16:22:11 +0900

    migration調整

--- db/migrate/20231018083243_create_items.rb
+++ db/migrate/20231018083243_create_items.rb
@@ -6,13 +6,11 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer    :category_id,           null: false   
       t.integer    :condition_id,          null: false
       t.integer    :del_fee_id,            null: false
-      
       t.integer    :prefecture_id,         null: false
       t.integer    :days_until_shipping_id,null: false
       t.integer    :price,                 null: false
       t.references :user, null: false,     foreign_key: true
     
-
       t.timestamps
     end
   end
