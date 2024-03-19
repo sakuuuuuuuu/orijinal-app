c0e16e0be3022393248f7260ca07d0a7ec6150b0
sakuuuuuuuu <themesong99@gmail.com>
2024-03-19 23:19:59 +0900

    再調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,6 @@ class Item < ApplicationRecord
   # , class_name: 'Prefecture'
   belongs_to :days_until_shipping
 
-
-
-  
   has_one :purchase_record # 商品購入機能実装で復活させる
   belongs_to :user
   has_one_attached :image
