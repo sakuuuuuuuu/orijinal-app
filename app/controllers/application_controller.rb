4da5ad665d5c92b84f6339d22bb053daee9111c1
sakuuuuuuuu <themesong99@gmail.com>
2024-04-06 23:15:40 +0900

    調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,6 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_last, :name_first, :name_last_kana,:name_first_kana,:birth_day])
   end
 end
-
 # before_action :configure_permitted_parameters, if: :devise_controller?
 
 #   protected
